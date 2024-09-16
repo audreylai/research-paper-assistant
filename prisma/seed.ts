b97ce3358b5377ba3fdbc74275b181a05d0239b8
Lucas Ma <51366352+lucasma8795@users.noreply.github.com>
2024-09-16 16:25:34 +0800

    misc: dbtest UI + seed db with faker

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
+
+function createRandomUser() {
+  return {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+  };
+}
 
 async function main() {
   const lucas = await prisma.user.upsert({
@@ -21,7 +29,18 @@ async function main() {
     },
   });
 
-  console.log({ lucas, audrey });
+  const users = faker.helpers.multiple(createRandomUser, { count: 10 });
+
+  users.map(async ({ name, email }) => {
+    await prisma.user.upsert({
+      where: { email: email },
+      update: {},
+      create: {
+        email: email,
+        name: name,
+      },
+    });
+  });
 }
 
 main()
